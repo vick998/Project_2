0344fe6a6fc5b94b779c9a2d94ed358711d0302d
Victor <viktor.vidovski@gmail.com>
2021-07-03 20:00:47 +0200

    Further finnicking

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AuctionListing, AuctionBid, ListingComment
+from .models import User,AuctionListing, AuctionBid, ListingComment
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(AuctionListing)
 admin.site.register(AuctionBid)
 admin.site.register(ListingComment)
