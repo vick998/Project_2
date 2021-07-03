bf59922f75ce667cdab1497e3bb872541491edef
Victor <viktor.vidovski@gmail.com>
2021-07-03 20:12:07 +0200

    Revert "Revert "Setting it straight.""
    This reverts commit 999de6c5d0c7981243ed80df0f3b8ca51c6fd591.

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -78,6 +78,60 @@ def listentry(request):
             return render(request, "auctions/listingentry.html", {
                 "message": "Listing already exists."
             })
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("listingpage", args=(flight_id,)))
     else:
         return render(request, "auctions/listingentry.html")
+
+def listingpage(request,listing_id):
+    listing = AuctionListing.objects.get(id=listing_id)
+    if request.method == "POST":
+        if AuctionListing.user_name == User.username:
+            username_bid = User.username
+            listingname_bid = listing.listingname
+            followbid = request.POST["newbid"]
+            try:
+                auctionbid = AuctionBid.objects.create(username_bid, listingname_bid, followbid)
+                auctionbid.save()
+            except IntegrityError:
+                return render(request, "auctions/listingpage.html", {
+                    "listing": listing,
+                    "message": "Bid already entered."
+                })
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/listingpage.html",{
+                "listing": listing,
+                "message": "Auction closed by owner"
+                })
+    else:
+        if AuctionListing.user_name == User.username:
+            user_match == True
+            return render(request, "auctions/listingpage.html",{
+                "listing": listing
+                })
+        else:
+            user_match == False
+            return render(request, "auctions/listingpage.html",{
+                "listing": listing
+                })
+
+def watchlist(request):
+    listings = list(AuctionListing.objects.all())
+    listingspec = []
+    for listing in listings:
+        if user.username == listing.user_name:
+            listingspec.append(listing)
+    if len(listingspec) == 0:
+        return render(request, "auctions/watchlist.html",{
+            "message":"No listings entered"
+            })
+    else:
+        return render(request, "auctions/watchlist.html",{
+            "listings":listingspec
+            })
+
+def category(request):
+    return render(request, "auctions/category.html", {
+        "listings" : list(AuctionListing.objects.all())
+        })
+
